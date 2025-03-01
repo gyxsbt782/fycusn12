16f235fbe9afcf478dba40411d0a40118a68ea65
huxsz <131564174+gyxsbt782@users.noreply.github.com>
2025-03-01 23:21:33 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || '18711f25-82d2-4868-8ac7-50d472fffb2b'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || 'a2843013-51ca-4557-add6-69876cfdd8c0'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'mkcusgt.yadssx.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMzU0N2YwMDg5NzkyNTg3MDFhNzc0MDdjMTdlNDBmMDkiLCJ0IjoiMTc0ZjY5ODAtZWQ0Yy00YmY1LWFkMzUtNmI1MDBmNGQ4MjU3IiwicyI6IllUUTRNakkxTUdNdFpUUmlPQzAwT0daa0xUazRZV010WW1VNE9EVXpORFEyT0dJeSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'fdtcys.dtjdhdtjft4ge4t.gq';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTIwZmQxYWNjNjhjNDU3MzI0MTE1ZjAzYWY3YzA5YTIiLCJ0IjoiMjAwYjQyYzYtMTEzZS00N2MyLWE5NzgtYTZlZDBjZWM5NDZhIiwicyI6Ik9HTTRNRFZtT1RVdFpXVmlaaTAwTldGbUxUbGlObU10T0RnMlltVmxZalF5TjJFeiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
